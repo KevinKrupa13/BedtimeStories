19b3d18dd0e6876db819c9efdf8d8d570efb0850
hmacht <hmacht88@gmail.com>
2021-12-21 14:47:43 -0500

    My starter files for the app

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from story import write_story, append_to_story, session_prompt
+
+def ghostWritter():
+    userStory = input("Enter your prompt: ")
+    print("\n\n#####################################\n#####################################\n#####################################")
+    print("------And so your tale begins--------\n\n")
+    #session_story = "The following is a spooky story written for kids, just in time for Halloween. Everyone always talks about the old house at the end of the street, but I couldn't believe what happened when I went inside."
+    session_story = "The following is a story written for children. It is about " + userStory 
+    story = ""
+    for i in range(0, 2):
+        story = write_story(session_story)
+        session_story = append_to_story(story, session_story)
+        print(story)
+    print("\n\n--------------The end----------------")
+    print("#####################################\n#####################################\n#####################################\n\n")
+
+
+
+if __name__ == "__main__":
+    ghostWritter()
