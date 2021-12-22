de507cda00ff83f8e234894fb1e5fbb6798761ea
KevinKrupa13 <kevinkrupa13@vt.edu>
2021-12-22 16:08:06 -0500

    smalled changes to story then added my key for testing

--- story.py
+++ story.py
@@ -5,7 +5,7 @@ import openai
 from flask import Flask, request
 
 load_dotenv()
-openai.api_key = os.environ.get('OPENAI_KEY')
+openai.api_key = os.environ.get('KEVOPENAI_KEY')
 completion = openai.Completion()
 
 session_prompt = "The following is a spooky story written for kids, just in time for Halloween. Everyone always talks about the old house at the end of the street, but I couldn't believe what happened when I went inside."
@@ -18,10 +18,11 @@ def write_story(session_story=None):
     response = openai.Completion.create(
       engine="davinci",
       prompt=prompt_text,
-      temperature=0.7,
+      temperature=0.9,
       max_tokens=96,
+      n=1,
       top_p=1,
-      frequency_penalty=0,
+      frequency_penalty=0.5,
       presence_penalty=0.3,
     )
     story = response['choices'][0]['text']
